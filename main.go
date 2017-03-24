2ed53cc8a1af23a532fcff24aab24c1647c4abd4
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2017-03-25 06:40:02 +1300

    init

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+		w.WriteHeader(http.StatusOK)
+		return
+	})
 }
